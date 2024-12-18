25ee4cfc3fa7a48940d00918a91d0136979f705e
gabimatatov <gavriel.matatov@veeva.com>
2024-12-18 23:35:12 +0200

    added funcitons display x/o on the board

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.example.tictactoe
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    enum class PlayerTurn {
+        CIRCLE,
+        CROSS
+    }
+
+    private var firstTurn = PlayerTurn.CROSS
+    private var currentTurn = PlayerTurn.CROSS
+
+    lateinit var binding: ActivityMainBinding
+    private var boardList = mutableListOf<Button>()
+
+    // onCreate Function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initBoard()
+    }
+
+    private fun initBoard() {
+        boardList.add(binding.buttonX00)
+        boardList.add(binding.buttonX01)
+        boardList.add(binding.buttonX02)
+        boardList.add(binding.buttonX10)
+        boardList.add(binding.buttonX11)
+        boardList.add(binding.buttonX12)
+        boardList.add(binding.buttonX20)
+        boardList.add(binding.buttonX21)
+        boardList.add(binding.buttonX22)
+    }
+
+    fun playerTapped(view: View) {
+        if (view !is Button)
+            return
+
+        addToBoard(view)
+    }
+
+    private fun addToBoard(button: Button){
+        if (button.text != "")
+            return
+        if (currentTurn == PlayerTurn.CIRCLE){
+            button.text = "O"
+            currentTurn = PlayerTurn.CROSS
+        }
+        else if (currentTurn == PlayerTurn.CROSS){
+            button.text = "X"
+            currentTurn = PlayerTurn.CIRCLE
         }
     }
 }
